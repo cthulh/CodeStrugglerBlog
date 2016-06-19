7a40792d0346d57c9d9ffd51d2acf3d083686568
cthulh <tryglavson@hotmail.co.uk>
2016-06-19 02:14:18 +0100

    Adding styles from the design project

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -5,11 +5,11 @@ class RegistrationsController < Devise::RegistrationsController
 	private
 
 	def sign_up_params
-		params.require(:user).permit(:role, :username, :name, :email, :password, :password_confirmation)
+		params.require(:user).permit(:admin, :username, :name, :email, :password, :password_confirmation)
 	end
 
 	def account_update_params
-		params.require(:user).permit(:role, :username, :name, :email, :password, :password_confirmation, :current_password)
+		params.require(:user).permit(:admin, :username, :name, :email, :password, :password_confirmation, :current_password)
 	end
 
 end
